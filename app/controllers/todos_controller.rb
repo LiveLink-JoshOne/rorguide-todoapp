434c03f099bb63c7b50dc4f1df4a094041029dc1
Joshua Empson <j.empson@livelinktechnology.net>
2021-05-25 19:53:33 +0100

    Add functionality to create new Todos and show them

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,27 @@
+class TodosController < ApplicationController
+
+  def show 
+    @todo = Todo.find(params[:id])
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save                                        ## if the save is successful
+      flash[:notice] = 'Your To-Do item was successfully created!'  ## add msg to flash
+      redirect_to todo_path(@todo)                       ## use prefix for todos#show
+    else                                                 ## if the save is unsuccessful
+      render 'new'                                       ## show same form if db refuses
+    end
+  end
+
+  private                                                ## THIS CONTROLLER ONLY
+    def todo_params
+      params.require(:todo).permit(:name, :description)  ## whitelisting params fields
+    end
+
+end
+
